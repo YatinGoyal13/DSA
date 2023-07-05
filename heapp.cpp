1d0550219d288f992a18219f818e42bc0efcd5a0
YatinGoyal13 <goyalyatin2002@gmail.com>
2023-07-05 23:02:54 +0530

    chnge

--- heapp.cpp
+++ heapp.cpp
@@ -37,7 +37,7 @@ public:
         {
             cout << arr[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     void deletefromheap()
     {
@@ -70,24 +70,24 @@ public:
         }
     }
 };
-void heapify(int arr[],int n,int i)
+void heapify(int arr[], int n, int i)
 {
-    int largest=i;
-    int left=2*i;
-    int right=2*i +1;
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
 
-    if(left<n && arr[left]>arr[largest])
+    if (left < n && arr[left] > arr[largest])
     {
-        largest=left;
+        largest = left;
     }
-    if(right<n && arr[right]>arr[largest])
+    if (right < n && arr[right] > arr[largest])
     {
-        largest=right;
+        largest = right;
     }
-    if(largest!=i)
+    if (largest != i)
     {
-        swap(arr[largest],arr[i]);
-        heapify(arr,n,largest);
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
     }
 }
 
@@ -102,15 +102,15 @@ int main()
     h.print();
     h.deletefromheap();
     h.print();
-    int arr[6]={-1,54,53,55,52,50};
-    int n=5;
-    for(int i=n/2 ;i>0;i--)
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+    for (int i = n / 2; i > 0; i--)
     {
-        heapify(arr,n,i);
+        heapify(arr, n, i);
     }
-    cout<<"array after heapify"<<endl;
-    for(int i=1;i<=n;i++)
+    cout << "array after heapify" << endl;
+    for (int i = 1; i <= n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
 }
